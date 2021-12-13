ff69f20dfc853c24e112fcac2c2ad8795468d912
mingzhixian <mingzhixian123@gmail.com>
2021-12-13 20:04:45 +0800

    12.13更新

--- c/实验/sanyuanzu.h
+++ c/实验/sanyuanzu.h
@@ -20,36 +20,54 @@ SYZ syzinit(int a[], int h, int l)
 		c.frist[x] = c.jd;
 		for (int i = 0; i < l; i++)
 		{
-			if (a[x * h + i] != 0)
+			if (a[x * l + i] != 0)
 			{
-				c.data[c.jd].data = a[x * h + i];
-				c.data[c.jd].h = x;
-				c.data[c.jd].l = i;
+				c.data[c.jd].data = a[x * l + i];
+				c.data[c.jd].h = x + 1;
+				c.data[c.jd].l = i + 1;
 				c.jd++;
 			}
 		}
 	}
 	return c;
 }
+void syztodata(SYZ a, int (*data)[])
+{
+	int h = a.jh;
+	int l = a.jl;
+	for (int x = 0; x < h * l; x++)
+	{
+		(*data)[x] = 0;
+	}
+	for (int i = 0; i < a.jd; i++)
+	{
+		(*data)[(a.data[i].h-1) * l + a.data[i].l-1] = a.data[i].data;
+	}
+}
 SYZ syzadd(SYZ a, SYZ b)
 {
 	SYZ c;
 	c.jh = a.jh;
 	c.jl = a.jl;
-	for (int i = 0; i < c.jh; i++)
+	if (a.jh != b.jh || a.jl != b.jl)
+	{
+		printf("行列数不相等不能相加。\n");
+		return c;
+	}
+	for (int i = 0; i < c.jh; i++) //第i行
 	{
 		one af = a.data[a.frist[i]]; //af,bf为该行第一个元素
 		one bf = b.data[b.frist[i]];
-		int ap = 1, bp = 1;
+		int ap = 1, bp = 1; //下一个
 		c.frist[i] = c.jd;
-		while (af.h == i && bf.h == i)
+		while (af.h == i + 1 && bf.h == i + 1)
 		{
 			if (af.l == bf.l)
 			{
-				c.jd++;
-				c.data[c.jd].h = i;
+				c.data[c.jd].h = i + 1;
 				c.data[c.jd].l = af.l;
 				c.data[c.jd].data = af.data + bf.data;
+				c.jd++;
 				af = a.data[a.frist[i] + ap];
 				bf = b.data[b.frist[i] + bp];
 				ap++;
@@ -57,49 +75,153 @@ SYZ syzadd(SYZ a, SYZ b)
 			}
 			else if (af.l < bf.l)
 			{
-				c.jd++;
-				c.data[c.jd].h = i;
+				c.data[c.jd].h = i + 1;
 				c.data[c.jd].l = af.l;
 				c.data[c.jd].data = af.data;
+				c.jd++;
 				af = a.data[a.frist[i] + ap];
 				ap++;
 			}
 			else if (af.l > bf.l)
 			{
-				c.jd++;
-				c.data[c.jd].h = i;
+				c.data[c.jd].h = i + 1;
 				c.data[c.jd].l = bf.l;
 				c.data[c.jd].data = bf.data;
+				c.jd++;
 				bf = b.data[b.frist[i] + bp];
 				bp++;
 			}
 		}
-		while (af.h == i)
+		while (af.h == i + 1)
 		{
-			c.jd++;
-			c.data[c.jd].h = i;
+			c.data[c.jd].h = i + 1;
 			c.data[c.jd].l = af.l;
 			c.data[c.jd].data = af.data;
+			c.jd++;
 			af = a.data[a.frist[i] + ap];
 			ap++;
 		}
-		while (bf.h == i)
+		while (bf.h == i + 1)
 		{
-			c.jd++;
-			c.data[c.jd].h = i;
+			c.data[c.jd].h = i + 1;
 			c.data[c.jd].l = bf.l;
 			c.data[c.jd].data = bf.data;
+			c.jd++;
 			bf = b.data[b.frist[i] + bp];
 			bp++;
 		}
 	}
 	return c;
 }
+SYZ syzsub(SYZ a, SYZ b)
+{
+	SYZ c;
+	c.jh = a.jh;
+	c.jl = a.jl;
+	if (a.jh != b.jh || a.jl != b.jl)
+	{
+		printf("行列数不相等不能相减。\n");
+		return c;
+	}
+	for (int i = 0; i < c.jh; i++) //第i行
+	{
+		one af = a.data[a.frist[i]]; //af,bf为该行第一个元素
+		one bf = b.data[b.frist[i]];
+		int ap = 1, bp = 1; //下一个
+		c.frist[i] = c.jd;
+		while (af.h == i + 1 && bf.h == i + 1)
+		{
+			if (af.l == bf.l)
+			{
+				c.data[c.jd].h = i + 1;
+				c.data[c.jd].l = af.l;
+				c.data[c.jd].data = af.data - bf.data;
+				if (c.data[c.jd].data)
+				{
+					c.jd++;
+				}
+				af = a.data[a.frist[i] + ap];
+				bf = b.data[b.frist[i] + bp];
+				ap++;
+				bp++;
+			}
+			else if (af.l < bf.l)
+			{
+				c.data[c.jd].h = i + 1;
+				c.data[c.jd].l = af.l;
+				c.data[c.jd].data = af.data;
+				c.jd++;
+				af = a.data[a.frist[i] + ap];
+				ap++;
+			}
+			else if (af.l > bf.l)
+			{
+				c.data[c.jd].h = i + 1;
+				c.data[c.jd].l = bf.l;
+				c.data[c.jd].data = -bf.data;
+				c.jd++;
+				bf = b.data[b.frist[i] + bp];
+				bp++;
+			}
+		}
+		while (af.h == i + 1)
+		{
+			c.data[c.jd].h = i + 1;
+			c.data[c.jd].l = af.l;
+			c.data[c.jd].data = af.data;
+			c.jd++;
+			af = a.data[a.frist[i] + ap];
+			ap++;
+		}
+		while (bf.h == i + 1)
+		{
+			c.data[c.jd].h = i + 1;
+			c.data[c.jd].l = bf.l;
+			c.data[c.jd].data = -bf.data;
+			c.jd++;
+			bf = b.data[b.frist[i] + bp];
+			bp++;
+		}
+	}
+	return c;
+}
+SYZ syzmul(SYZ a, SYZ b)
+{
+	SYZ c;
+	c.jh = a.jh;
+	c.jl = b.jl;
+	if (a.jl != b.jh)
+	{
+		printf("此两个矩阵无法相乘。\n");
+		return c;
+	}
+	int adata[a.jd * a.jl], bdata[b.jd * b.jl];
+	syztodata(a, &adata);
+	syztodata(b, &bdata);
+	for (int i = 0; i < a.jh; i++)
+	{
+		c.frist[i] = c.jd;
+		for (int x = 0; x < b.jl; x++)
+		{
+			int num = 0;
+			for (int y = 0; y < a.jl; y++)
+			{
+				num += adata[i * a.jl + y] * bdata[y * b.jl + x];
+			}
+			if (num)
+			{
+				c.data[c.jd].h = i+1;
+				c.data[c.jd].l = x+1;
+				c.data[c.jd].data = num;
+				c.jd++;
+			}
+		}
+	}
+}
 void syzprt(SYZ a)
 {
 	for (int i = 0; i < a.jd; i++)
 	{
-		printf("第%d行第%d列，数值为%d\n",a.data[i].);
+		printf("第%d行第%d列，数值为%d\n", a.data[i].h, a.data[i].l, a.data[i].data);
 	}
-	
 }
--- c/实验/作业2-3.cpp
+++ c/实验/作业2-3.cpp
@@ -1,17 +1,54 @@
 #include <stdio.h>
 #include "sanyuanzu.h"
-void randata(int (*data)[])
+#include <random>
+#include <time.h>
+SYZ randata(int x, int y, int k)
 {
-
+	srand(time(NULL));
+	int n = x * y;
+	int d[n], data[n] = {0};
+	for (int i = 0; i < n; i++)
+		d[i] = i;
+	for (int i = n; i > 0; i--)
+	{ /* 打乱*/
+		int a = i - 1;
+		int b = rand() % i;
+		if (a != b)
+		{
+			int t = d[a];
+			d[a] = d[b];
+			d[b] = t;
+		}
+	}
+	for (int i = 0; i < x; i++)
+	{
+		for (int j = 0; j < y; j++)
+		{
+			data[i * y + j] = (d[i * y + j] >= (n - k)) ? 1 : 0;
+		}
+	}
+	SYZ c = syzinit(data, x, y);
+	return c;
 }
 int main(int argc, char const *argv[])
 {
-	int data1[6*6];
-	randata(&data1);
-	SYZ a=syzinit(data1,6,6);
-	int data2[6*6];
-	randata(&data2);
-	SYZ b=syzinit(data2,6,6);
-	SYZ c=syzadd(a,b);
+	printf("输入 m、p、n、k1、k2：(空格隔开)\n");
+	int m, p, n, k1, k2;
+	scanf("%d %d %d %d %d", &m, &p, &n, &k1, &k2);
+	SYZ a = randata(m, p, k1);
+	SYZ b = randata(p, n, k2);
+	SYZ c = syzadd(a, b);
+	SYZ d = syzsub(a, b);
+	SYZ e = syzmul(a, b);
+	syzprt(a);
+	printf("\n");
+	syzprt(b);
+	printf("\n");
+	syzprt(c);
+	printf("\n");
+	syzprt(d);
+	printf("\n");
+	syzprt(e);
+
 	return 0;
 }
